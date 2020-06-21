39f4cebf09b00c27348fc645ddf4727777dd3825
東海林マックスウィリアム透 <tokairinmaxwilliamtohoru@TMWTMacBook-Pro.local>
2020-06-21 17:01:36 +0900

    質問1、2、４修正再提出

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.push("斎藤")
+  names << "斎藤"
   puts names
 end
 
@@ -14,7 +14,7 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-  puts array1.push(array2)
+  puts array1 + array2
 end
 
 def q3
@@ -28,7 +28,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.delete(nil)
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -46,7 +46,7 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = [10, 20, 30, 40, 50]
 end
 
 def q7
