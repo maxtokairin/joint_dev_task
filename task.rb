51dc3a4ccc782c092b318c034c963c421bf83e3f
東海林マックスウィリアム透 <tokairinmaxwilliamtohoru@TMWTMacBook-Pro.local>
2020-06-23 17:11:29 +0900

    質問Q6〜10、Q12を回答

--- task.rb
+++ task.rb
@@ -46,14 +46,15 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-  numbers2 = [10, 20, 30, 40, 50]
+  numbers2 = numbers1.map! {|x| x *10 }
+  p numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -62,6 +63,9 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  upper_case_programming_languages = %w(ruby php python javascript)
+  programming_languages.map(&:capitalize!)
+  upper_case_programming_languages.map(&:upcase!)
 
   # 以下は変更しないで下さい
   p programming_languages
@@ -70,30 +74,41 @@ end
 
 def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
-
+ names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
   # 以下に回答を記載
-
+  
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
+  foods.each do |food|
+    if food.include? ("うに")
+      puts "好物です"
+    else
+      puts "まぁまぁ好きです"
+    end
+  end
 
+  
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+ 
+  end
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  puts data[:user][:name]
 end
 
 def q13
