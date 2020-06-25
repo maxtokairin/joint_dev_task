96f913a3b4c4ac9f53e91fa363eb246b207eb1d7
東海林マックスウィリアム透 <tokairinmaxwilliamtohoru@TMWTMacBook-Pro.local>
2020-06-26 07:59:53 +0900

    質問Q８、１１を修正、再提出

--- task.rb
+++ task.rb
@@ -63,7 +63,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages.map(&:capitalize)
+  programming_languages.map(&:capitalize!)
   upper_case_programming_languages =
   programming_languages.map(&:upcase)
 
@@ -94,10 +94,9 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports.flatten!.uniq!
   puts "ユーザーの趣味一覧" 
-  sports.each.with_index(1) do |sport, i|
-    puts "No#{i} #{sport}"
+  sports.flatten!.uniq!.each.with_index(1) do |sport, i|
+  puts "No#{i} #{sport}"
   end
 end
 
