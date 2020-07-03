d34083b4d3cc51d9b57908e14978eddcf02d2420
東海林マックスウィリアム透 <tokairinmaxwilliamtohoru@TMWTMacBook-Pro.local>
2020-07-03 19:46:07 +0900

    質問14、15を修正、再提出

--- task.rb
+++ task.rb
@@ -120,9 +120,9 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-  data.each_key do |key|
-    puts "#{key}"
-  end
+  array = data.keys
+  p array
+
 end
 
 def q15
@@ -130,8 +130,16 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  puts data1.fetch("age", :"OK")
-  puts data2.fetch("age", :"NG")
+  if data1.has_key? (:age) then
+    puts "OK"
+  else
+    puts "NG"
+  end
+  if data2.has_key? (:age) then
+    puts "OK"
+  else
+    puts "NG"
+  end
 end
 
 def q16
