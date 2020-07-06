a98919d915d296be19b4b297e8ee8c5c5e4c0a1d
東海林マックスウィリアム透 <tokairinmaxwilliamtohoru@TMWTMacBook-Pro.local>
2020-07-06 20:49:22 +0900

    質問15修正、再提出

--- task.rb
+++ task.rb
@@ -130,16 +130,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.has_key? (:age) then
-    puts "OK"
-  else
-    puts "NG"
-  end
-  if data2.has_key? (:age) then
-    puts "OK"
-  else
-    puts "NG"
-  end
+  puts data1.has_key?(:age) ? "OK" : "NG"
+  puts data2.has_key?(:age) ? "OK" : "NG"
 end
 
 def q16
